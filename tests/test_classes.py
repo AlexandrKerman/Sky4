cf1f7c50ffee126f2a8a767b6c247d00c129f4c9
Kerbin Original <aakolomeycev@gmail.com>
2026-02-26 14:30:19 +0300

    Дописаны тесты

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,3 +1,8 @@
+from unittest.mock import patch
+
+from src import classes
+
+
 def test_product(product_obj):
     assert product_obj.name == "product_1"
     assert product_obj.description == "Some product 1 description"
@@ -8,6 +13,34 @@ def test_product(product_obj):
 def test_category(category_obj):
     assert category_obj.name == "category_1"
     assert category_obj.description == "Some category 1 description"
-    assert category_obj.products == []
-    assert category_obj.product_count == 0
+    assert category_obj.product_count == 1
     assert category_obj.category_count == 1
+
+
+def test_new_product(category_obj):
+    pr_data = {
+        "name": "Samsung Galaxy C23 Ultra",
+        "price": 1200,
+        "description": "256GB, Серый цвет, 200MP камера",
+        "quantity": 2,
+    }
+    first_test_product = classes.Product.new_product(pr_data, [category_obj])
+    assert first_test_product.price == 1200
+
+
+def test_price(product_obj):
+    with patch("builtins.input", return_value="y"):
+        product_obj.price = 10
+        assert product_obj.price == 10
+
+    with patch("builtins.input", return_value="n"):
+        product_obj.price = 5
+        assert product_obj.price == 10
+
+
+def test_add_product(category_obj, product_obj):
+    assert len(category_obj.products) == 1
+
+    category_obj.add_product(product_obj)
+    assert category_obj.product_objects[0].name == "product_1"
+    assert len(category_obj.products) == 2
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,7 +34,9 @@ def test_convert_dict_list():
 
     assert utils.convert_dict_list([]) == []
     assert result_data[0].name == "Смартфоны"
-    assert result_data[0].products[0].name == "Samsung Galaxy C23 Ultra"
+    assert result_data[0].product_objects[0].name == "Samsung Galaxy C23 Ultra"
+    assert result_data[0].products[0] == "Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт."
     assert isinstance(result_data[0], Category)
     assert isinstance(result_data[0].products, list)
-    assert isinstance(result_data[0].products[0], Product)
+    assert isinstance(result_data[0].products[0], str)
+    assert isinstance(result_data[0].product_objects[0], Product)
